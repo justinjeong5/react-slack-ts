e180152de613fd0b49050fabcd69f62e852cd752
justinjeong <justin.jeong5@gmail.com>
2021-03-05 15:32:53 +0900

    add: router

--- client/layouts/App.tsx
+++ client/layouts/App.tsx
@@ -1,10 +1,16 @@
 import React from 'react'
+import {Switch, Route, Redirect} from 'react-router-dom'
+
+import SignIn from '@pages/SignIn'
+import SignUp from '@pages/SignUp'
 
 function App() {
-  return (
-    <div>
-      App
-    </div>
+  return ( 
+    <Switch>      
+      <Redirect exact path='/' to='/login' />
+      <Route path='/login' component={SignIn} />
+      <Route path='/signup' component={SignUp} />
+    </Switch>
   )
 }
 
